76588c7d8729e46d96371a3825b7f3c7f2a877d5
Grondin Alexandre <117280087+Miro974@users.noreply.github.com>
2023-03-15 17:22:19 -0700

    Bases obstacles

--- Assets/_MyAsset/Scripts/GestionCollision.cs
+++ Assets/_MyAsset/Scripts/GestionCollision.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class GestionCollision : MonoBehaviour
 {
+    // Attributs
+    private bool _touche;
     //Méthodes privées
+    private void Start()
+    {
+        _touche = false;
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        if (collision.gameObject.tag == "Player")
+        {
+            if (!_touche)
+            {
+                gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+                _touche = true;
+            }
+        }
+        
     }
 }
