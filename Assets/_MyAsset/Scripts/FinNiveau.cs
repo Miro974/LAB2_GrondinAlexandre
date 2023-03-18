b7232e3c3db3a70a5b82ae188d1b153e8bef43e6
Grondin Alexandre <117280087+Miro974@users.noreply.github.com>
2023-03-17 19:23:44 -0700

    Meh

--- Assets/_MyAsset/Scripts/FinNiveau.cs
+++ Assets/_MyAsset/Scripts/FinNiveau.cs
@@ -40,21 +40,24 @@ public class FinNiveau : MonoBehaviour
             {
                 int contacts = _gestionJeu.GetPointage();
                 // Temps + pénalités Niveau 1
-                float tpsNiv1 = Time.time - _gestionJeu.GetTpsNiv1();
-                float tpsTotalNiv1 = tpsNiv1 + _gestionJeu.GetContactNiv1();
+                //float tpsNiv1 = Time.time - _gestionJeu.GetTpsNiv1();
+                float tpsTotalNiv1 = _gestionJeu.GetTpsNiv1() + _gestionJeu.GetContactNiv1();
                 // Temps + pénalités Niveau 2
-                float tpsNiv2 = Time.time - tpsNiv1;
+                float tpsNiv2 = Time.time - _gestionJeu.GetTpsNiv1();
                 int contactNiv2 = _gestionJeu.GetPointage() - _gestionJeu.GetContactNiv1();
+                //Debug.Log(_gestionJeu.GetPointage().ToString());
+                //Debug.Log(_gestionJeu.GetContactNiv1().ToString());
+                //Debug.Log((_gestionJeu.GetPointage() - _gestionJeu.GetContactNiv1()).ToString());
                 float tpsTotalNiv2 = tpsNiv2 + contactNiv2;
                 // Temps + pénalités Niveau 3
-                //float tpsNiv3 = Time.time - (tpsNiv1 + tpsNiv2);
-                //int contactNiv3 = (_gestionJeu.GetContactNiv1() + _gestionJeu.GetContactNiv2())  - _gestionJeu.GetPointage();
-                //float tpsTotalNiv3 = tpsNiv3 + contactNiv3;
+                float tpsNiv3 = Time.time - (_gestionJeu.GetTpsNiv1() + tpsNiv2);
+                int contactNiv3 = _gestionJeu.GetPointage() /*- (_gestionJeu.GetContactNiv1() + contactNiv2)*/;
+                float tpsTotalNiv3 = tpsNiv3 + contactNiv3;
 
                 // Bilan de fin de partie
                 Debug.Log("Fin de partie!!");
                 // Niveau 1
-                Debug.Log("Votre temps pour le niveau 1 est de : " + tpsNiv1.ToString("f2") + " secondes");
+                Debug.Log("Votre temps pour le niveau 1 est de : " + _gestionJeu.GetTpsNiv1().ToString("f2") + " secondes");
                 Debug.Log("Au niveau 1 vous avez touché : " + _gestionJeu.GetContactNiv1().ToString() + " obstacles");
                 Debug.Log("Temps total du niveau 1 : " + tpsTotalNiv1.ToString("f2") + " secondes");
                 // Niveau 2
@@ -62,9 +65,9 @@ public class FinNiveau : MonoBehaviour
                 Debug.Log("Au niveau 2 vous avez touché : " + contactNiv2.ToString() + " obstacles");
                 Debug.Log("Temps total du niveau 2 : " + tpsTotalNiv2.ToString("f2") + " secondes");
                 // Niveau 3
-                //Debug.Log("Votre temps pour le niveau 3 est de : " + tpsNiv3.ToString("f2") + " secondes");
-                //Debug.Log("Au niveau 3 vous avez touché : " + contactNiv3.ToString() + " obstacles");
-                //Debug.Log("Temps total du niveau 3 : " + tpsTotalNiv3.ToString("f2") + " secondes");
+                Debug.Log("Votre temps pour le niveau 3 est de : " + tpsNiv3.ToString("f2") + " secondes");
+                Debug.Log("Au niveau 3 vous avez touché : " + contactNiv3.ToString() + " obstacles");
+                Debug.Log("Temps total du niveau 3 : " + tpsTotalNiv3.ToString("f2") + " secondes");
 
                 Debug.Log("Votre temps total pour les trois niveaux est de : " + (tpsTotalNiv1 + tpsTotalNiv2).ToString("f2"));
                 _player.finJoueur();
@@ -72,14 +75,16 @@ public class FinNiveau : MonoBehaviour
             else
             {
                 // Charger la scène suivante
-                if(numScene == 1)
+                
                     _gestionJeu.SetNiv1(_gestionJeu.GetPointage(), Time.time);
-                else if (numScene == 2)
-                    _gestionJeu.SetNiv2(_gestionJeu.GetPointage(), Time.time);
+                    //Debug.Log(_gestionJeu.GetContactNiv1().ToString());
+                
+                    _gestionJeu.SetNiv2((_gestionJeu.GetPointage() - _gestionJeu.GetContactNiv1()), (Time.time - _gestionJeu.GetTpsNiv1()));
 
 
                 SceneManager.LoadScene(numScene + 1);
             }
+            
         }
 
     }
